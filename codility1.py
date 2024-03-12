f63725e4dbb25eb7b61f802d949b2160f0e8c0ff
King <briomwaura@gmail.com>
2024-03-12 12:52:40 +0300

    assignment complete

---
+++ codility1.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+def solution(A, D):
+    balance = 0
+    fee = 0
+    total_income = 0
+    prev_month = 0
+    
+    for i in range(len(A)):
+        date = datetime.strptime(D[i], "%Y-%m-%d")
+        current_month = date.month
+        
+        if A[i] < 0:
+            balance += A[i]
+            fee += 1
+        else:
+            balance += A[i]
+            total_income += A[i]
+        
+        if current_month != prev_month:
+            if fee < 100 or abs(fee) < 3:
+                balance -= 5
+            fee = 0
+            prev_month = current_month
+    
+    if fee < 100 or abs(fee) < 3:
+        balance -= 5
+    
+    return balance - (5 * 12) + total_income
